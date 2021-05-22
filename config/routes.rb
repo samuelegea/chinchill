ad9a7e13548922818d0b3a399b6abe2bf660666d
samuelegea <samuelegea@hotmail.com>
2021-05-22 01:11:37 -0300

    fix for production deploy

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :products
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   
