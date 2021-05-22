6ba9b86eb9b41a7cb09dc500e3a16417f08ac97c
samuelegea <samuelegea@hotmail.com>
2021-05-22 00:27:20 -0300

    finilized products frontend

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,57 +6,11 @@ class ProductsController < InheritedResources::Base
         @products = Product.all
         @current_user = current_user
     end
-
-    def show
-        @product = Product.find(params[:id])
-    end
-
-    def new
-        @product = Product.new
-    end
-
-    def create
-        @product = Product.new(params[:product])
-        if @product.save
-          flash[:success] = "Product successfully created"
-          redirect_to @product
-        else
-          flash[:error] = "Something went wrong"
-          render 'new'
-        end
-    end
-
-    def edit
-        @product = Product.find(params[:id])
-    end
-    
-
-    def update
-        @object = Object.find(params[:id])
-        if @object.update_attributes(params[:object])
-          flash[:success] = "Object was successfully updated"
-          redirect_to @object
-        else
-          flash[:error] = "Something went wrong"
-          render 'edit'
-        end
-    end
-
-    def destroy
-        @product = Product.find(params[:id])
-        if @product.destroy
-            flash[:success] = 'Product was successfully deleted.'
-            redirect_to products_url
-        else
-            flash[:error] = 'Something went wrong'
-            redirect_to products_url
-        end
-    end
    
   private
 
     def product_params
-      params.require(:product).permit(:name, :description, :price, :nutrionalinformation)
+      params.require(:product).permit(:name, :description, :price)
     end
 
 end
